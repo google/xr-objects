43fa1f68466c61db9e80bc50c67b731dc9e50d32
dogadogan <mdogadogan@gmail.com>
2024-03-01 17:09:33 +0300

    Image query files

--- XRObjects/Assets/XRObjects/Scripts/ImageQuery.cs
+++ XRObjects/Assets/XRObjects/Scripts/ImageQuery.cs
@@ -14,67 +14,93 @@ using UnityEngine.Networking;
 using System.Text;
 using System.Linq;
 using TMPro;
-// using System.Web;
+
+// Class for making HTTP calls to the Gemini API
+// don't forget to input your personal Gemini API key
 
 public class ImageQuery : MonoBehaviour
 {
+
+  // Get your Gemini API key here: https://makersuite.google.com/app/apikey
+  private string apiKey = "enter your key here";
+  private string queryURL;
+
   [HideInInspector] public Texture2D Texture2DImageOfObject;
 
   // variable to store the object ID specific to the object
   [HideInInspector] public string objectConversationID;
+  
 
-  // set up data structure for the query JSON - this is for the initial call
-  [Serializable]
-  public class ImageQueryRequestJSON
+  // for the input
+  [System.Serializable]
+  public class RequestContent
+  {
+    public RequestPart[] parts;
+  }
+
+  [System.Serializable]
+  public class RequestPart
   {
     public string text;
-    public model_specJson[] model_spec;
-    [Serializable]
-    public class model_specJson
-    {
-      public string modelKey;
-      public string modelId;
-    }
-
-    public string inference_hints;
-
-    public image_sourceJson[] image_source;
-    [Serializable]
-    public class image_sourceJson
-    {
-      public string raw_image;
-    }
+    public InlineData inline_data;
   }
 
-  // set up data structure for returned Json Response
-  [Serializable]
-  public class ImageQueryResponseJSON
+  [System.Serializable]
+  public class InlineData
   {
-    // public List<AnnotateImageResponse> responses;
-    public String text;
-    public String conversationId;
-    public String conversationLogs;
-    public float latencySec;
-    public String previousLog;
+    public string mime_type;
+    public string data;
   }
 
-  // set up data structure for the FOLLOW-UP queries (once you know the conversation ID)
-  [Serializable]
-  public class ImageFollowUpQueryRequestJSON
+  [System.Serializable]
+  public class RequestBody
+  {
+    public RequestContent[] contents;
+  }
+
+  // for the output
+
+  [System.Serializable]
+  public class GeminiAPIResponse
+  {
+    public Candidate[] candidates;
+  }
+
+  [System.Serializable]
+  public class Candidate
+  {
+    public Content content;
+    public string finishReason;
+    public int index;
+    public SafetyRating[] safetyRatings;
+  }
+
+  [System.Serializable]
+  public class Content
+  {
+    public Part[] parts;
+    public string role;
+  }
+
+  [System.Serializable]
+  public class Part
   {
     public string text;
-    public model_specJson[] model_spec;
-    [Serializable]
-    public class model_specJson
-    {
-      public string modelKey;
-      public string modelId;
-    }
-
-    public string inference_hints;
-    public string conversation_id;
-
   }
+
+  [System.Serializable]
+  public class SafetyRating
+  {
+    public string category;
+    public string probability;
+  }
+
+  [System.Serializable]
+  public class PromptFeedback
+  {
+    public SafetyRating[] safetyRatings;
+  }
+
 
   public class CoroutineWithOutput
   {
@@ -96,6 +122,13 @@ public class ImageQuery : MonoBehaviour
       this.coroutineTarget = coroutineTarget;
       this.coroutine = owner.StartCoroutine(RunCoroutine());
     }
+  }
+
+
+  void Awake()
+  {
+
+    queryURL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision:generateContent?key=" + apiKey;
 
   }
   void Start()
@@ -103,8 +136,68 @@ public class ImageQuery : MonoBehaviour
     // test if internet connection works
     // StartCoroutine(HTTPGet("https://www.google.com/"));
     // StartCoroutine(HTTPPost("https://httpbin.org/post", ""));
-
   }
+
+  public string CreateJsonData(string promptText, Texture2D texture2DImageOfObject)
+  {
+    string base64Image = System.Convert.ToBase64String(texture2DImageOfObject.EncodeToJPG());
+
+    // Manually constructing the JSON string to ensure correct format
+    string jsonData = "{"
+                    + "\"contents\":[{"
+                    + "\"parts\":["
+                    + "{\"text\":\"" + promptText + "\"},"
+                    + "{\"inline_data\": { \"mime_type\":\"image/jpeg\", \"data\":\"" + base64Image + "\"}}"
+                    + "]"
+                    + "}]"
+                    + "}";
+    return jsonData;
+  }
+
+  // Sends a POST request to the Gemini API
+  IEnumerator HTTPPost(string url, string bodyJsonString)
+  {
+    var request = new UnityWebRequest(url, "POST");
+    Debug.Log("Gemini bodyJsonString: " + bodyJsonString);
+    byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
+    Debug.Log("Gemini bodyRaw: " + bodyRaw);
+
+    request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+    request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+    //request.SetRequestHeader("Accept", "application/json");
+    request.SetRequestHeader("Content-Type", "application/json");
+
+    Debug.Log("Gemini SendWebRequest ");
+    yield return request.SendWebRequest();
+    Debug.Log("Gemini SendWebRequest done ");
+
+    if (request.result != UnityWebRequest.Result.Success)
+    {
+      Debug.LogError($"Gemini Error: {request.error}");
+    }
+    else
+    {
+      Debug.Log($"Gemini Raw Response: {request.downloadHandler.text}");
+      // Deserialize JSON response
+      GeminiAPIResponse response = JsonUtility.FromJson<GeminiAPIResponse>(request.downloadHandler.text);
+
+      Debug.Log($"Gemini Response: {response}");
+
+      // Extract and log the text content from the first candidate's first part
+      if (response.candidates != null && response.candidates.Length > 0 && response.candidates[0].content.parts != null && response.candidates[0].content.parts.Length > 0)
+      {
+        string extractedText = response.candidates[0].content.parts[0].text;
+        Debug.Log($"Gemini --- Extracted Text: {extractedText}");
+        yield return extractedText;
+      }
+      else
+      {
+        Debug.Log("Gemini --- No text content found in the response.");
+        yield return "Gemini No text content found in the response.";
+      }
+    }
+  }
+
 
   // method for running the initial query
   public IEnumerator RunInitialImageQuery(string initialPrompt = "Don't answer as complete sentence! Name/brand + model/type/species? (no size information)")
@@ -112,36 +205,39 @@ public class ImageQuery : MonoBehaviour
 
     //create JsonString
     // string initialPrompt = "Don't answer as complete sentence! Name/brand + model/type/species? (no size information)";
-    var bodyJsonString = CreateInitialJsonData(initialPrompt, Texture2DImageOfObject);
+    var bodyJsonString = CreateJsonData(initialPrompt, Texture2DImageOfObject);
 
     Debug.Log("HTTP bodyJsonString: " + bodyJsonString);
 
-
-    CoroutineWithOutput cd = new CoroutineWithOutput(this, HTTPPost("https://nlu-service.sandbox.googleapis.com/run_model_sync", bodyJsonString));
+    CoroutineWithOutput cd = new CoroutineWithOutput(this, HTTPPost(queryURL, bodyJsonString));
     yield return cd.coroutine;
-    Debug.Log("result is " + cd.output);  //  'success' or 'fail'
+    Debug.Log("GEMINI cd.output = " + cd.output);  //  'success' or 'fail'
 
     if (cd.output != null)
     {
       transform.GetComponent<SetupObjectProxy>().updateMetadata(cd.output.ToString());
+      Debug.Log("GEMINI cd.output.ToString() = " + cd.output.ToString());
     }
 
+    //StartCoroutine(HTTPPost("https://httpbin.org/post", bodyJsonString));
   }
-
 
   // method for running the initial query but with a output callback
   public IEnumerator RunInitialImageQueryWithOutput(string prompt, System.Action<string> callback)
   {
 
+    Debug.Log("COMPARE ---1 ");
+
     //create JsonString
     // string initialPrompt = "Don't answer as complete sentence! Name/brand + model/type/species? (no size information)";
-    var bodyJsonString = CreateInitialJsonData(prompt, Texture2DImageOfObject);
+    var bodyJsonString = CreateJsonData(prompt, Texture2DImageOfObject);
 
-    // Debug.Log("COMPARE RunInitialImageQueryWithOutput HTTP bodyJsonString: " + bodyJsonString);
+    Debug.Log("COMPARE RunInitialImageQueryWithOutput HTTP bodyJsonString: " + bodyJsonString);
 
-    CoroutineWithOutput cd = new CoroutineWithOutput(this, HTTPPost("https://nlu-service.sandbox.googleapis.com/run_model_sync", bodyJsonString));
+
+    CoroutineWithOutput cd = new CoroutineWithOutput(this, HTTPPost(queryURL, bodyJsonString));
     yield return cd.coroutine;
-    // Debug.Log("COMPARE RunInitialImageQueryWithOutput result is " + cd.output);  //  'success' or 'fail'
+    Debug.Log("COMPARE RunInitialImageQueryWithOutput result is " + cd.output);  //  'success' or 'fail'
 
     callback(cd.output.ToString());
 
@@ -150,93 +246,19 @@ public class ImageQuery : MonoBehaviour
   // method for running a FOLLOW-UP query
   public IEnumerator RunFollowUpImageQuery(string prompt, System.Action<string> callback)
   {
+
     //create JsonString
-    var bodyJsonString = CreateFollowUpJsonData(prompt);
+    var bodyJsonString = CreateJsonData(prompt, Texture2DImageOfObject);
 
-    //Debug.Log("HTTP bodyJsonString: " + bodyJsonString);
+    Debug.Log("HTTP bodyJsonString: " + bodyJsonString);
 
-    CoroutineWithOutput cd = new CoroutineWithOutput(this, HTTPPost("https://nlu-service.sandbox.googleapis.com/run_model_sync", bodyJsonString));
+
+    CoroutineWithOutput cd = new CoroutineWithOutput(this, HTTPPost(queryURL, bodyJsonString));
     yield return cd.coroutine;
-    Debug.Log("result is " + cd.output);  //  'success' or 'fail'
+    Debug.Log("Result is " + cd.output);  //  'success' or 'fail'
 
     callback(cd.output.ToString());
 
-  }
-
-  // call for the first query (without knowing conversationId)
-  public string CreateInitialJsonData(string promptText, Texture2D Texture2DImageOfObject)
-  {
-    string jsonData;
-
-    // encode image as JPEG -> base64
-    // Texture2DImageOfObject.SetPixels(pixels);
-    // texture2D.Apply(false); // Not required. Because we do not need to be uploaded it to GPU
-    byte[] ImageJPG = Texture2DImageOfObject.EncodeToJPG();
-    string ImageBase64 = System.Convert.ToBase64String(ImageJPG);
-
-    var _imageQueryRequestJSON = new ImageQueryRequestJSON()
-    {
-
-      // text = "What do you see in this photo? Give a short answer, not a full sentence. Do not put a period at the end of the answer. If you see a product, you can mention its brand, for example: San Pellegrino Naturali Aranciata sparkling drink",
-      text = promptText,
-      model_spec = new[]
-        {
-                // If the number of arrays increases, add more
-                new ImageQueryRequestJSON.model_specJson()
-                {
-                modelKey =  "mms_extractor",
-                modelId = "model_mms_fresh"
-                }
-            },
-      inference_hints = "use_pali_vqa=True",
-      image_source = new[]
-        {
-                // If the number of arrays increases, add more
-                new ImageQueryRequestJSON.image_sourceJson()
-                {
-                raw_image =  ImageBase64 //image_bytes
-                }
-            }
-
-    };
-
-    jsonData = JsonUtility.ToJson(_imageQueryRequestJSON, false); // false = pretty print
-    Debug.Log(jsonData);
-    return jsonData;
-  }
-
-  // call for the FOLLOW-UP queries (when conversationId is known)
-  public string CreateFollowUpJsonData(string promptText)
-  {
-    string jsonData;
-
-    // encode image as JPEG -> base64
-    // Texture2DImageOfObject.SetPixels(pixels);
-    // texture2D.Apply(false); // Not required. Because we do not need to be uploaded it to GPU
-    byte[] ImageJPG = Texture2DImageOfObject.EncodeToJPG();
-    string ImageBase64 = System.Convert.ToBase64String(ImageJPG);
-
-    var _imageQueryRequestJSON = new ImageFollowUpQueryRequestJSON()
-    {
-
-      // text = "What do you see in this photo? Give a short answer, not a full sentence. Do not put a period at the end of the answer. If you see a product, you can mention its brand, for example: San Pellegrino Naturali Aranciata sparkling drink",
-      text = promptText,
-      model_spec = new[]
-        {
-                // If the number of arrays increases, add more
-                new ImageFollowUpQueryRequestJSON.model_specJson()
-                {
-                modelKey =  "mms_extractor",
-                modelId = "model_mms_fresh"
-                }
-            },
-      inference_hints = "use_pali_vqa=True",
-      conversation_id = objectConversationID
-    };
-
-    jsonData = JsonUtility.ToJson(_imageQueryRequestJSON, false); // false = pretty print
-    Debug.Log(jsonData);
-    return jsonData;
   }
 
   IEnumerator HTTPGet(string testURL)
@@ -258,53 +280,5 @@ public class ImageQuery : MonoBehaviour
       }
     }
   }
-
-
-  IEnumerator HTTPPost(string url, string bodyJsonString)
-  {
-    var request = new UnityWebRequest(url, "POST");
-
-    // get rid of [ and ] from the string
-    bodyJsonString = bodyJsonString.Replace("[", string.Empty);
-    bodyJsonString = bodyJsonString.Replace("]", string.Empty);
-
-    Debug.Log("HTTPS Cleaned Json: " + bodyJsonString);
-
-    byte[] bodyRaw = System.Text.Encoding.Default.GetBytes(bodyJsonString);
-
-    request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
-    request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-
-    request.SetRequestHeader("Accept", "application/json");
-    request.SetRequestHeader("Content-Type", "application/json");
-
-    yield return request.SendWebRequest();
-
-    var returnedJson = request.downloadHandler.text;
-    Debug.Log("HTTPS request.responseCode: " + request.responseCode);
-    Debug.Log("HTTPS request.text: " + returnedJson);
-    Debug.Log("HTTPS request.text shorter:" + returnedJson.Replace("\n", "")); //.Replace(" ", ""));
-
-    ImageQueryResponseJSON returnedJsonDeserialized = JsonUtility.FromJson<ImageQueryResponseJSON>(returnedJson);
-
-    // get the LLM response text
-    var responseLLM = returnedJsonDeserialized.text;
-    objectConversationID = returnedJsonDeserialized.conversationId;
-
-    // transform.GetComponent<SetupObjectProxy>().updateMetadata(response);
-
-    if (string.IsNullOrEmpty(request.error))
-    {
-      Debug.Log("HTTPS success");
-      yield return responseLLM;
-    }
-    else
-    {
-      Debug.Log("HTTPS error!");
-      yield return "error";
-    }
-
-  }
-
 
 }
